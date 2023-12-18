9f3e1f330a14f1990c26b15fd06bac635391cb82
Daisy Tonui <tonuidaisy@gmail.com>
2023-12-18 06:41:26 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -18,10 +18,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print("")
-    return ret
-
-
-# Example usage:
-my_list = [1, 2, 3, 4, 5]
-num_printed = safe_print_list(my_list, 3)
-print("Number of elements printed:", num_printed)
+    return (ret)

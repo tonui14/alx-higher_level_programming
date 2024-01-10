a2eb9d3e17facc38c80ca2ea9a8caced61496ab1
Daisy Tonui <tonuidaisy@gmail.com>
2024-01-09 16:04:15 -0800

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a string-to-JSON function."""
+"""Defines string-to-JSON function."""
 import json
 
 
 def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
+    """Return the JSON representation of string object."""
     return json.dumps(my_obj)
